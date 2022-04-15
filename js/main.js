85b63d5327b9c3cfddd9346ef9a642b161853fcc
Alaskapablo <you@example.com>
2022-04-15 12:32:11 -0300

    prog-att

--- js/main.js
+++ js/main.js
@@ -1,58 +1,57 @@
-const newTask = document.querySelector("#input-js");
 const btnAdd = document.querySelector("#btn-add-js");
-const form = document.querySelector("#form-add-js");
-const listTask = document.querySelector("#task-list-js");
-const checkBtn = document.querySelector(".check-btn");
-const clearBtn = document.querySelector(".clear-btn");
 
 btnAdd.addEventListener("click", (event)=>{
     event.preventDefault()
+    // console.log(btnAdd)
 
+    var form = document.querySelector("#form-add-js")
+    var taskObj = getInfoFromInput(form)
+    // console.log(taskObj.task)
+   
+    const elementLi = createLi(taskObj)
+   
+
+    var taskList = document.querySelector("#task-list-js")
+    taskList.appendChild(elementLi)
+
+    form.reset()
+})
+
+
+// var taskValid = 
+
+// function validTask(taskObj){
+//     if(taskObj.task < 0){
+       
+//     }
+// }
+
+function createLi(taskObj){
     const elementLi = document.createElement('li')
+    elementLi.classList.add("task-item")
+
+    const checkTask = document.createElement('input');
+    checkTask.type = 'checkbox';
+    checkTask.classList.add("input-task")
+
     const textTask = document.createElement('p')
-    const iconDelete = document.createElement('span')
+    textTask.classList.add("task-text")
 
-    textTask.innerText = newTask.value;
-    iconDelete.innerText = "🗑"
-    
-    if(textTask.innerText.trim() === ''){
-        
-        newTask.classList.add('error');
-        newTask.addEventListener('animationend', event =>{
-            newTask.classList.remove('error');
-        })
-    }else{
-        listTask.appendChild(elementLi)
-        elementLi.appendChild(textTask)
-        elementLi.appendChild(iconDelete)
-        form.reset()
-    }
-    newTask.focus()
+    const iconDel = document.createElement('span')
+    iconDel.classList.add("del-task")
 
-
-    iconDelete.addEventListener('click', () => {
-        elementLi.remove();
-        CheckMarc();
-      })
-      CheckMarc()
-    })
-
-
-
-
-
-function CheckMarc(){
-    let allTasks = document.querySelectorAll('p')
-    let check = []
-
-    allTasks.forEach(task => {
-        if(!task.classList.contains("checked")){
-          check.push(task)
-        };
-    })
+    textTask.textContent = taskObj.task
+    iconDel.innerText = "🗑"
+    elementLi.appendChild(checkTask)
+    elementLi.appendChild(textTask)
+    elementLi.appendChild(iconDel)
+   
+    return elementLi;
 }
 
-
-
-
-
+function getInfoFromInput(form){
+    var taskObj = {
+        task: form.task.value
+    }
+    return taskObj
+}
